2946dd37f82805a015c1e564430ee5db0a1e902f
whl <ewanghailong@vip.qq.com>
2024-08-16 13:33:05 +0800

    "init"

---
+++ android/app/src/main/kotlin/com/example/flutter_desktop_fluentui_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_desktop_fluentui_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  //去除默认边框
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await flutter_acrylic.Window.initialize();
+  await flutter_acrylic.Window.hideWindowControls();
+
+  await WindowManager.instance.ensureInitialized();
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitleBarStyle(
+      TitleBarStyle.hidden,
+      windowButtonVisibility: false,
+    );
+    await windowManager.setMinimumSize(const Size(500, 600));
+    await windowManager.show();
+    await windowManager.setPreventClose(true);
+    await windowManager.setSkipTaskbar(false);
+  });
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FluentApp(
+      title: "Test",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  int _selectedIndex = 0;
+
+  final List<NavigationPaneItem> _items = [
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('Home'),
+      body: const SizedBox.shrink(),
+    ),
+    PaneItem(
+      icon: const Icon(FluentIcons.settings),
+      title: const Text('Settings'),
+      body: const SizedBox.shrink(),
+    ),
+  ];
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      appBar: NavigationAppBar(
+        title: () {
+          return const DragToMoveArea(
+            child: Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text("test"),
+            ),
+          );
+        }(),
+        automaticallyImplyLeading: false,
+        actions: const Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          WindowButtons(),
+        ]),
+      ),
+      pane: NavigationPane(
+        displayMode: PaneDisplayMode.auto,
+        selected: _selectedIndex,
+        onChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: _items,
+      ),
+    );
+  }
+
+  @override
+  void onWindowClose() async {
+    bool isPreventClose = await windowManager.isPreventClose();
+    if (isPreventClose && mounted) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return ContentDialog(
+            title: const Text('Confirm close'),
+            content: const Text('Are you sure you want to close this window?'),
+            actions: [
+              FilledButton(
+                child: const Text('Yes'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  windowManager.destroy();
+                },
+              ),
+              Button(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FluentThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
